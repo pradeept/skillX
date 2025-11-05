4834d68ff1241352286d6fbb91abf76b97449f41
pradeept <pradeep.tarakar17@gmail.com>
2025-11-05 19:50:04 +0530

    feat: add drizzle ORM and database configuration
    - Updated package.json to include drizzle-orm and drizzle-kit dependencies.
    - Added scripts for database generation and pushing.
    - Created db.ts for database connection using drizzle ORM with PostgreSQL.
    - Added schema.ts file for future schema definitions.

---
+++ server/drizzle.config.ts
@@ -0,0 +1,14 @@
+// drizzle configuration file 
+import {defineConfig} from "drizzle-kit"
+import 'dotenv/config'
+
+export default defineConfig({
+    out:'./src/drizzle/', //path to store migrations
+    schema:'./src/drizzle/schema.ts', 
+    dialect:'postgresql',
+    dbCredentials:{
+        url:process.env.DATABASE_URL as string
+    },
+    verbose:true, //logs migration changes
+    strict:true //warns during migration generation if migration causes BREAKING changes like deleting or altering table
+})
